4bf26343e5e12a0209d85223a04ebfcd76e8bc72
Toks Fawibe <toksfawibe@Tokss-MacBook-Pro.local>
2024-02-18 12:35:53 -0600

    feat: finished code

--- functions/http_reader.py
+++ functions/http_reader.py
@@ -2,22 +2,18 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-# Initialize the S3 client
 s3 = boto3.client('s3')
 
-# Your S3 bucket name
 bucket_name = os.environ['BUCKET_NAME']
 
 def lambda_handler(event, context):
     try:
-        # Initialize paginator for handling potentially truncated responses
         paginator = s3.get_paginator('list_objects_v2')
         pages = paginator.paginate(Bucket=bucket_name, Prefix='')
 
         latest_object_key = None
         latest_last_modified = None
 
-        # Iterate over each page of objects
         for page in pages:
             if 'Contents' in page:
                 for obj in page['Contents']:
@@ -25,9 +21,7 @@ def lambda_handler(event, context):
                         latest_last_modified = obj['LastModified']
                         latest_object_key = obj['Key']
 
-        # Check if we found an object
         if latest_object_key:
-            # Get the content of the most recent object
             latest_object = s3.get_object(Bucket=bucket_name, Key=latest_object_key)
             latest_content = latest_object['Body'].read().decode('utf-8')
 
